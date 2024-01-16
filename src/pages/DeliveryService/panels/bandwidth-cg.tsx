46f15d1cb3a66e0b59e687f1e10c6e99274de105
The Anh Nguyen <ntheanh201@gmail.com>
2024-01-16 11:29:21 +0700

    migrate ds scene using query variable

--- src/pages/DeliveryService/panels/bandwidth-cg.tsx
+++ src/pages/DeliveryService/panels/bandwidth-cg.tsx
@@ -1,51 +1,23 @@
 import { SceneQueryRunner, PanelBuilders } from '@grafana/scenes';
 import { INFLUXDB_DATASOURCES_REF } from '../../../constants';
-import { DeliveryServiceCustomObject } from '../DSCustomObject';
 
-export const getBandwidthByCGPanel = ({ customObject }: { customObject: DeliveryServiceCustomObject }) => {
-  const defaultBandwidthByCacheGroupQuery = {
+export const getBandwidthByCGPanel = () => {
+  const bandwidthByCacheGroupQuery = {
     refId: 'A',
-    query: `SELECT mean(value) FROM "monthly"."kbps.cg.1min" WHERE cachegroup != 'all' and $timeFilter GROUP BY time(60s), cachegroup`,
+    query: `SELECT mean(value) FROM "monthly"."kbps.cg.1min" WHERE deliveryservice='$deliveryservice' AND cachegroup != 'all' and $timeFilter GROUP BY time(60s), cachegroup`,
     rawQuery: true,
     resultFormat: 'time_series',
     alias: '$tag_cachegroup',
   };
 
-  const queryRunner3 = new SceneQueryRunner({
+  const qr = new SceneQueryRunner({
     datasource: INFLUXDB_DATASOURCES_REF.DELIVERYSERVICE_STATS,
-    queries: [defaultBandwidthByCacheGroupQuery],
-  });
-
-  queryRunner3.addActivationHandler(() => {
-    const sub = customObject.subscribeToState((newState) => {
-      queryRunner3.setState(
-        !!newState.name
-          ? {
-              queries: [
-                {
-                  ...queryRunner3.state.queries[0],
-                  query:
-                    'SELECT mean(value) FROM "monthly"."kbps.cg.1min" WHERE deliveryservice=\'' +
-                    newState.name +
-                    "' and cachegroup != 'all' and $timeFilter GROUP BY time(60s), cachegroup",
-                },
-              ],
-            }
-          : {
-              queries: [defaultBandwidthByCacheGroupQuery],
-            }
-      );
-      queryRunner3.runQueries();
-    });
-
-    return () => {
-      sub.unsubscribe();
-    };
+    queries: [bandwidthByCacheGroupQuery],
   });
 
   return PanelBuilders.timeseries()
     .setTitle('Bandwidth by CacheGroup')
-    .setData(queryRunner3)
+    .setData(qr)
     .setOption('legend', { showLegend: true, calcs: ['max'] })
     .setCustomFieldConfig('axisCenteredZero', true)
     .setCustomFieldConfig('spanNulls', true)
--- src/pages/DeliveryService/panels/bandwidth.tsx
+++ src/pages/DeliveryService/panels/bandwidth.tsx
@@ -1,54 +1,24 @@
 import { SceneQueryRunner, PanelBuilders } from '@grafana/scenes';
 import { INFLUXDB_DATASOURCES_REF } from '../../../constants';
-import { DeliveryServiceCustomObject } from '../DSCustomObject';
 
-export const getBandwidthPanel = ({ customObject }: { customObject: DeliveryServiceCustomObject }) => {
+export const getBandwidthPanel = () => {
   const defaultBandwidthQuery = {
     refId: 'A',
-    query: `SELECT mean(value) FROM "monthly"."kbps.ds.1min" WHERE cachegroup = 'total'  and $timeFilter GROUP BY time(60s), deliveryservice ORDER BY asc`,
+    query: `SELECT mean(value) FROM "monthly"."kbps.ds.1min" WHERE deliveryservice='$deliveryservice' AND cachegroup = 'total'  and $timeFilter GROUP BY time(60s), deliveryservice ORDER BY asc`,
     rawQuery: true,
     resultFormat: 'time_series',
     alias: '$tag_deliveryservice',
     measurement: 'bw',
   };
 
-  const queryRunner1 = new SceneQueryRunner({
+  const qr = new SceneQueryRunner({
     datasource: INFLUXDB_DATASOURCES_REF.DELIVERYSERVICE_STATS,
     queries: [defaultBandwidthQuery],
   });
 
-  queryRunner1.addActivationHandler(() => {
-    const sub = customObject.subscribeToState((newState) => {
-      queryRunner1.setState(
-        !!newState.name
-          ? {
-              queries: [
-                {
-                  ...queryRunner1.state.queries[0],
-                  query:
-                    'SELECT mean(value) FROM "monthly"."kbps.ds.1min" WHERE deliveryservice=\'' +
-                    newState.name +
-                    `' and cachegroup = 'total' and $timeFilter GROUP BY time(60s), deliveryservice ORDER BY asc`,
-                  tags: {
-                    deliveryservice: newState.name,
-                  },
-                },
-              ],
-            }
-          : {
-              queries: [defaultBandwidthQuery],
-            }
-      );
-      queryRunner1.runQueries();
-    });
-
-    return () => {
-      sub.unsubscribe();
-    };
-  });
-
   return PanelBuilders.timeseries()
     .setTitle('Bandwidth')
+    .setData(qr)
     .setOption('legend', { showLegend: true, calcs: ['max'] })
     .setCustomFieldConfig('axisCenteredZero', true)
     .setUnit('bps')
